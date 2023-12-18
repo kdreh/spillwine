f51daffbf0da5376200747bb3c88dfc0b57f7a2f
K-Andre Harris <dre@sustainible.io>
2023-12-18 11:49:19 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch:'full'},
+  { path: '**', redirectTo: '/home', pathMatch:'full' },
   {path:'home', component:HomeComponent},
   {path: 'contact', component:ContactComponent}
 ];
