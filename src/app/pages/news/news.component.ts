2fa39e9c5c44d1d83f627fe5ee755b45d04cb071
K-Andre Harris <dre@sustainible.io>
2024-01-19 13:37:05 -0500

    updated source

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class NewsComponent {
 
-  url: string = 'https://mesearch.ai/';
+  url: string = 'https://mesearch.ai/spillwinepublishing';
   urlSafe: SafeResourceUrl | undefined;
 
   constructor(public sanitizer: DomSanitizer) {}
