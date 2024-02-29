7c4f50433a2e2369eb53b3ad20e806f4134a4b9f
K-Andre Harris <dre@sustainible.io>
2024-02-29 14:46:30 -0500

    Removed the news navigation via comment

--- src/app/navigation/page-route.ts
+++ src/app/navigation/page-route.ts
@@ -1,6 +1,9 @@
 export const pageRoute = [
     {page:'Home', route:'/home'},
-    {page:'News',route:'/news'},
+   /*  
+   TODO if Martha or Art wants to display the news again remove this comment
+    {page:'News',route:'/news'}, 
+    */
     {page:'Knowledge Market',route:'/knowledge'},
     // {page:'About', route:'/about'},
     // {page:'Contact', route:'/contact'},
