06c5dbe330c81908b6b9e0c1eefb4ec68e4f00f5
K-Andre Harris <dre@sustainible.io>
2024-03-01 09:10:14 -0500

    style mobile based navigation

--- src/app/navigation/page-route.ts
+++ src/app/navigation/page-route.ts
@@ -1,10 +1,10 @@
 export const pageRoute = [
     {page:'Home', route:'/home'},
     // {page:'About', route:'/about'},
-   /*  
-   TODO if Martha or Art wants to display the news again remove this comment
-    {page:'News',route:'/news'}, 
-    */
+    
+    /*TODO if Martha or Art wants to display the news again remove this comment */
+    // {page:'News',route:'/news'}, 
+   
     {page:'Knowledge Market',route:'/knowledge'},
 
     // {page:'Contact', route:'/contact'},
